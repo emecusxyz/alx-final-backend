f7fdd76a8f3c378fc314fd9f764c22bb3fd8ad47
emecusxyz <112563553+emecusxyz@users.noreply.github.com>
2024-07-29 21:45:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors({ origin: "https://alx-final-frontend.onrender.com", credentials: true }));
+app.use(cors({ origin: "https://alx-final-frontend-1.onrender.com", credentials: true }));
 
 connectDB();
 
